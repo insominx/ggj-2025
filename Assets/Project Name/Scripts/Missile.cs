ceae6f789fdfed4c0f7475c34bd8130a4ef9ff52
Edward Leon <edleon761@gmail.com>
2025-01-24 19:15:16 -0800

    Checkpoint

---
+++ Assets/Project Name/Scripts/Missile.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+
+    public GameObject bomb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
+
