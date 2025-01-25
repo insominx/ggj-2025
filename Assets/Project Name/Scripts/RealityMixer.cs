6ebac6df6d42361717672e6b3381c487a6ec1c01
Dan McCue <daniel.mccue@woven-planet.global>
2025-01-24 23:48:51 -0800

    Start a RealityMixer class and experiment with placing things in the virtual world

---
+++ Assets/Project Name/Scripts/RealityMixer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class RealityMixer : MonoBehaviour
+{
+    // Editor/data/prefab interface.
+    public GameObject floor;
+
+    void Start()
+    {
+        // Procedurally create a floor.
+    }
+
+    // Update is called once per frame.
+    void Update()
+    {
+    }
+}
