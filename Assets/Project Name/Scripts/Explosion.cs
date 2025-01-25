a0efd3ebac9422047bb182d9eaf7cf4a163110ed
Edward Leon <edleon761@gmail.com>
2025-01-25 14:17:48 -0800

    Explosions change size similar to missile command

--- Assets/Project Name/Scripts/Explosion.cs
+++ Assets/Project Name/Scripts/Explosion.cs
@@ -5,17 +5,29 @@ public class Explosion : MonoBehaviour
 
     Vector3 scaleChange = new Vector3 (10f, 10f, 10f);
 
-    // Makes sure the explosion explodes in 3 seconds
-    void Start()
-    {
-        Destroy(gameObject, 3);
-    }
+    private bool growing = true;
 
     // Update is called once per frame
     void Update()
     {
 
         this.transform.localScale += (scaleChange * Time.deltaTime);
+
+        if (growing)
+        {
+            if (this.transform.localScale.x > 15f)
+            {
+                scaleChange = new Vector3 (-10f, -10f, -10f);
+                growing = false;
+            }
+        }
+        else
+        {
+            if (this.transform.localScale.x < 1f)
+            {
+                Destroy(this.gameObject);
+            }
+        }
 
 
     }
