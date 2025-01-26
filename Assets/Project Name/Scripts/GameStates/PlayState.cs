63fcb943b29aa3e9824e7822366bdb06787bd9d1
Dan McCue <daniel.mccue@woven-planet.global>
2025-01-25 23:20:05 -0800

    Add enemy spawn points

--- Assets/Project Name/Scripts/GameStates/PlayState.cs
+++ Assets/Project Name/Scripts/GameStates/PlayState.cs
@@ -9,6 +9,7 @@ class PlayState : GameState
 
     // State management.
     List<GameObject> cities = new();
+    List<GameObject> enemies = new();
     float timeWhenLastMissileSeen;
 
     public PlayState(GameManager gameManager)
@@ -20,10 +21,16 @@ class PlayState : GameState
     public override void Start()
     {
         realityMixer = Object.FindFirstObjectByType<RealityMixer>();
-        foreach (var pose in realityMixer.GetSpawnPoints())
+        foreach (var pose in realityMixer.GetCitySpawnPoints())
         {
             var city = SpawnBase(pose);
             cities.Add(city);
+        }
+
+        foreach (var pose in realityMixer.GetEnemySpawnPoints())
+        {
+            var enemy = SpawnEnemy(pose);
+            enemies.Add(enemy);
         }
 
         // Let's say we've seen a missile now, and we'll update this later.
@@ -37,6 +44,12 @@ class PlayState : GameState
             GameObject.Destroy(city);
         }
         cities.Clear();
+
+        foreach (var enemy in enemies)
+        {
+            GameObject.Destroy(enemy);
+        }
+        enemies.Clear();
     }
 
     public override void Update()
@@ -71,11 +84,24 @@ class PlayState : GameState
         var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
         // Position and scale it.
-        var cubeSideLength = 0.1f;
-        cube.transform.localScale = new Vector3(cubeSideLength, cubeSideLength, cubeSideLength);
-        cube.transform.SetPositionAndRotation(pose.position + new Vector3(0, cubeSideLength / 2, 0), pose.rotation);
+        var sideLength = 0.1f;
+        cube.transform.localScale = new Vector3(sideLength, sideLength, sideLength);
+        cube.transform.SetPositionAndRotation(pose.position + new Vector3(0, sideLength / 2, 0), pose.rotation);
 
         // Ready!
         return cube;
+    }
+
+    private GameObject SpawnEnemy(Pose pose)
+    {
+        var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        // Position and scale it.
+        var sideLength = 0.1f;
+        sphere.transform.localScale = new Vector3(sideLength, sideLength, sideLength);
+        sphere.transform.SetPositionAndRotation(pose.position + pose.forward * sideLength / 2, pose.rotation);
+
+        // Ready!
+        return sphere;
     }
 }
