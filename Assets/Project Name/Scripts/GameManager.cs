7f811ed074bbf4df4d2bff17996567d6490343b4
Dan McCue <daniel.mccue@woven-planet.global>
2025-01-26 00:20:42 -0800

    move spawning logic out to game manager (to make it easier to create and destroy in different game states)

--- Assets/Project Name/Scripts/GameManager.cs
+++ Assets/Project Name/Scripts/GameManager.cs
@@ -8,8 +8,8 @@ public class GameManager : MonoBehaviour
 {
     // Editor/data/prefab interface.
     [Header("References)")]
-    public GameObject prefabMissileSpawner; // Enemy missile spawner.
-    public GameObject prefabCity; // You defend these.
+    [SerializeField] GameObject prefabMissileSpawner; // Enemy missile spawner.
+    [SerializeField] GameObject prefabCity; // You defend these.
 
     // States.
     public GameState[] gameStates;
@@ -18,6 +18,10 @@ public class GameManager : MonoBehaviour
     // State management.
     int currentGameStateIdx = 0;
     GameState currentState { get { return gameStates[currentGameStateIdx]; } }
+
+    // Game information.
+    public List<GameObject> cities = new();
+    public List<GameObject> enemies = new();
 
     // Public Unity interface. /////////////////////////////////////////////////
 
@@ -75,5 +79,58 @@ public class GameManager : MonoBehaviour
     public void ReportDefeat()
     {
         gameConclusion.SetText("Defeat!");
+    }
+
+    public GameObject SpawnBase(Pose pose)
+    {
+        //var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        var sideLength = 0.1f;
+        var position = pose.position + new Vector3(0, sideLength / 2, 0);
+        var city = GameObject.Instantiate(prefabCity, position, pose.rotation);
+
+        // Position and scale it.
+        city.transform.localScale = new Vector3(sideLength, sideLength, sideLength);
+        //city.transform.SetPositionAndRotation(zz, pose.rotation);
+
+        cities.Add(city);
+
+        return city;
+    }
+
+    public GameObject SpawnEnemy(Pose pose)
+    {
+        //var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        var sideLength = 0.1f;
+        var position = pose.position + pose.forward * sideLength / 2;
+        var enemy = GameObject.Instantiate(prefabMissileSpawner, position, pose.rotation);
+
+        // Tell spawner not to collide with itself.
+        //enemy.GetComponent<MissileSpawner>().parentObject = enemy;
+
+        // Position and scale it.
+        enemy.transform.localScale = new Vector3(sideLength, sideLength, sideLength);
+        //enemy.transform.SetPositionAndRotation(zz, pose.rotation);
+
+        enemies.Add(enemy);
+
+        return enemy;
+    }
+
+    public void DestroyAllCities()
+    {
+        foreach (var city in cities)
+        {
+            GameObject.Destroy(city);
+        }
+        cities.Clear();
+    }
+
+    public void DestroyAllEnemies()
+    {
+        foreach (var enemy in enemies)
+        {
+            GameObject.Destroy(enemy);
+        }
+        enemies.Clear();
     }
 }
--- Assets/Project Name/Scripts/GameStates/PlayState.cs
+++ Assets/Project Name/Scripts/GameStates/PlayState.cs
@@ -8,8 +8,6 @@ class PlayState : GameState
     RealityMixer realityMixer;
 
     // State management.
-    List<GameObject> cities = new();
-    List<GameObject> enemies = new();
     float timeWhenLastMissileSeen;
 
     public PlayState(GameManager gameManager)
@@ -23,14 +21,12 @@ class PlayState : GameState
         realityMixer = Object.FindFirstObjectByType<RealityMixer>();
         foreach (var pose in realityMixer.GetCitySpawnPoints())
         {
-            var city = SpawnBase(pose);
-            cities.Add(city);
+            var city = gameManager.SpawnBase(pose);
         }
 
         foreach (var pose in realityMixer.GetEnemySpawnPoints())
         {
-            var enemy = SpawnEnemy(pose);
-            enemies.Add(enemy);
+            var enemy = gameManager.SpawnEnemy(pose);
         }
 
         // Let's say we've seen a missile now, and we'll update this later.
@@ -39,17 +35,8 @@ class PlayState : GameState
 
     public override void Stop()
     {
-        foreach (var city in cities)
-        {
-            GameObject.Destroy(city);
-        }
-        cities.Clear();
-
-        foreach (var enemy in enemies)
-        {
-            GameObject.Destroy(enemy);
-        }
-        enemies.Clear();
+        gameManager.DestroyAllCities();
+        gameManager.DestroyAllEnemies();
     }
 
     public override void Update()
@@ -62,7 +49,7 @@ class PlayState : GameState
         // Two conditions for game being over -- either cities are all gone or waves are done.
 
         // Check for cities being all gone.
-        var defeat = !cities.Any();
+        var defeat = !gameManager.cities.Any();
 
         // We win if there have been no missiles for 5 or 10 seconds.
         var victory = Time.time - timeWhenLastMissileSeen > 10.0f;
@@ -77,36 +64,5 @@ class PlayState : GameState
         }
 
         return victory || defeat;
-    }
-
-    private GameObject SpawnBase(Pose pose)
-    {
-        //var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        var sideLength = 0.1f;
-        var position = pose.position + new Vector3(0, sideLength / 2, 0);
-        var city = GameObject.Instantiate(gameManager.prefabCity, position, pose.rotation);
-
-        // Position and scale it.
-        city.transform.localScale = new Vector3(sideLength, sideLength, sideLength);
-        //city.transform.SetPositionAndRotation(zz, pose.rotation);
-
-        return city;
-    }
-
-    private GameObject SpawnEnemy(Pose pose)
-    {
-        //var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        var sideLength = 0.1f;
-        var position = pose.position + pose.forward * sideLength / 2;
-        var enemy = GameObject.Instantiate(gameManager.prefabMissileSpawner, position, pose.rotation);
-
-        // Tell spawner not to collide with itself.
-        //enemy.GetComponent<MissileSpawner>().parentObject = enemy;
-
-        // Position and scale it.
-        enemy.transform.localScale = new Vector3(sideLength, sideLength, sideLength);
-        //enemy.transform.SetPositionAndRotation(zz, pose.rotation);
-
-        return enemy;
     }
 }
