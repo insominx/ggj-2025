aaaf65f086006c86245923994e6bb149f9bb3ca4
Dan McCue <daniel.mccue@woven-planet.global>
2025-01-26 02:31:56 -0800

    missiles only explode when they hit cities or bubbles

--- Assets/Project Name/Scripts/Missile.cs
+++ Assets/Project Name/Scripts/Missile.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
-
     public GameObject bomb;
     public float speed;
     public Vector3 targetDirection;
@@ -17,29 +16,25 @@ public class Missile : MonoBehaviour
     {
         // Script to have the missile move to the target direction
         this.gameObject.transform.position += targetDirection * Time.deltaTime;
-
     }
-    
+
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject != spawner)
+        // Be more selective -- we only want to explode on hitting a) a target or b) a bubble.
+        var other = collision.gameObject;
+        if (other != spawner && (other.tag == "City" || other.tag == "Bubble"))
         {
             Explode();
         }
-
     }
-    
+
     void OnTriggerEnter(Collider other)
     {
-        
         // Checks if the collider has the explosion script
-        if(other.gameObject.GetComponent<Explosion>() != null && other.gameObject != spawner)
+        if (other.gameObject.GetComponent<Explosion>() != null && other.gameObject != spawner)
         {
-            
             Explode();
-
         }
-
     }
 
     void Explode()
@@ -47,7 +42,4 @@ public class Missile : MonoBehaviour
         Instantiate(bomb, this.transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
-    
-
 }
-
