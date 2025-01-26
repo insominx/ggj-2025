b44309e72971390f44f6a656df0feba1a44fe728
Dan McCue <daniel.mccue@woven-planet.global>
2025-01-26 02:31:20 -0800

    scaled explosions way down

--- Assets/Project Name/Scripts/Explosion.cs
+++ Assets/Project Name/Scripts/Explosion.cs
@@ -2,33 +2,33 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-
-    Vector3 scaleChange = new Vector3 (10f, 10f, 10f);
+    //Vector3 scaleChange = new Vector3 (10f, 10f, 10f);
+    Vector3 scaleChange = new Vector3 (0.1f, 0.1f, 0.1f);
 
     private bool growing = true;
 
     // Update is called once per frame
     void Update()
     {
-
         this.transform.localScale += (scaleChange * Time.deltaTime);
 
         if (growing)
         {
-            if (this.transform.localScale.x > 15f)
+            //if (this.transform.localScale.x > 15f)
+            if (this.transform.localScale.x > 0.2f)
             {
-                scaleChange = new Vector3 (-10f, -10f, -10f);
+                //scaleChange = new Vector3 (-10f, -10f, -10f);
+                scaleChange = new Vector3 (-0.2f, -0.2f, -0.2f);
                 growing = false;
             }
         }
         else
         {
-            if (this.transform.localScale.x < 1f)
+            //if (this.transform.localScale.x < 1f)
+            if (this.transform.localScale.x < 0.01f)
             {
                 Destroy(this.gameObject);
             }
         }
-
-
     }
 }
