f7c816a0c775898f06769b5e389376bb3d6f307d
Edward Leon <edleon761@gmail.com>
2025-01-26 00:24:25 -0800

    Checkpoint

---
+++ Assets/Project Name/Scripts/Bubble.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Game Object will destroy itself at some point dependent on it's size
+    IEnumerator DestroyBubble()
+    {
+        yield return new WaitForSeconds(this.gameObject.transform.localScale.x * 10f);
+        Destroy(this.gameObject);
+    }
+    
+}
